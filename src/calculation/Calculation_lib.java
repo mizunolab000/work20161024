76ea0d163c64c80405fe710396cf7d3ab2cadf20
mizunolab000 <mizunolab000@gmail.com>
2016-10-31 16:39:41 +0900

    20161024 submit

---
+++ src/calculation/Calculation_lib.java
@@ -0,0 +1,16 @@
+package calculation;
+
+public class Calculation_lib {
+	
+	private int m,n;
+
+	public Calculation_lib(int m, int n) {
+		this.m = m;
+		this.n = n;
+	}
+	
+	public int getPlus(){
+		return m+n;
+	}
+
+}
